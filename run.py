5ff997b4ddc8392ce4e0c82ec343b661ab5eef81
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2026-03-31 18:33:52 -0700

    add lume-pva wrapper

---
+++ run.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from lume_pva.runner import Runner
+from lume_torch.models import TorchModel
+from lume_torch.base import LUMETorchModel
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run the model as either a standlone server, or as a client consuming PVs from an external source.')
+    parser.add_argument('--remote', action='store_true', help='Configure input PVs as monitors for remote PVs')
+    args = parser.parse_args()
+
+    model = LUMETorchModel(
+        torch_model=TorchModel("model_config.yaml")
+    )
+    
+    config = Runner.generate_config(model=model, remote_inputs=args.remote)
+    
+    runner = Runner(
+        model=model,
+        config=Runner.generate_config(model=model, remote_inputs=args.remote)
+    )
+    print("Running model...")
+    runner.run()
